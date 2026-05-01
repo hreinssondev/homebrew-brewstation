3d5700d50f35ed71d99f37466e01e54ee478dbb6
hreinssondev <hreinssondev@gmail.com>
2026-05-01 18:28:36 +0200

    Update BrewStation to 1.4

--- Casks/brewstation.rb
+++ Casks/brewstation.rb
@@ -2,7 +2,7 @@ cask "brewstation" do
   version "1.4"
   sha256 "7f31a0c5c45cdb5252f508a4e4439a49eeace4d4e62bea291cca093b83a0268b"
 
-  url "https://github.com/hreinssondev/brewstation/releases/download/#{version}/Brestation.dmg"
+  url "https://github.com/hreinssondev/brewstation/releases/download/#{version}/Brewstation.dmg"
   name "BrewStation"
   desc "Homebrew GUI manager with snapshot saving and restoring"
   homepage "https://github.com/hreinssondev/brewstation"
